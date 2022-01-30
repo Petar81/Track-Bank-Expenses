dcadd656ffcaabe9c551f3b460caaa98f9de75da
Petar <baki_pfc@yahoo.com>
2022-01-30 04:07:01 +0100

    add loading spinner to indicate fetching of transaction history

--- lib/screens/transactions_history.dart
+++ lib/screens/transactions_history.dart
@@ -16,6 +16,7 @@ class _TransactionsHistoryState extends State<TransactionsHistory> {
   }
 
   Map<dynamic, dynamic> values = {};
+  bool _isLoading = true;
 
   getDeposits() async {
     DatabaseReference ref = FirebaseDatabase.instance.ref("transactions");
@@ -26,166 +27,187 @@ class _TransactionsHistoryState extends State<TransactionsHistory> {
     Map<dynamic, dynamic> data = event.snapshot.value as Map<dynamic, dynamic>;
     values = data;
 
-    setState(() {});
+    setState(() {
+      _isLoading = !_isLoading;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('View transactions history'),
-      ),
-      body: Card(
-        child: SizedBox(
-          height: double.infinity,
-          child: ListView.separated(
-            padding: const EdgeInsets.all(8),
-            itemCount: values.length,
-            itemBuilder: (BuildContext context, int index) {
-              String key = values.keys.elementAt(index);
-              return SizedBox(
-                height: 100,
-                //color: Colors.amber.shade300,
-                child: ListTile(
-                  leading: values[key]['balanceAfterTransaction'] <
-                          values[key]['balanceBeforeTransaction']
-                      ? Icon(
-                          Icons.south_west_rounded,
-                          color: Colors.red.shade300,
-                          size: 30,
-                        )
-                      : Icon(
-                          Icons.north_east_rounded,
-                          color: Colors.green.shade300,
-                          size: 30,
+    return _isLoading == true
+        ? Scaffold(
+            appBar: AppBar(
+              title: const Text('View transactions history'),
+            ),
+            body: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text('View transactions history'),
+            ),
+            body: Card(
+              child: SizedBox(
+                height: double.infinity,
+                child: ListView.separated(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: values.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    String key = values.keys.elementAt(index);
+                    return SizedBox(
+                      height: 110,
+                      //color: Colors.amber.shade300,
+                      child: ListTile(
+                        leading: values[key]['balanceAfterTransaction'] <
+                                values[key]['balanceBeforeTransaction']
+                            ? Icon(
+                                Icons.south_west_rounded,
+                                color: Colors.red.shade300,
+                                size: 30,
+                              )
+                            : Icon(
+                                Icons.north_east_rounded,
+                                color: Colors.green.shade300,
+                                size: 30,
+                              ),
+                        title: Text(
+                            '${values[key]['transactionDate']} @ ${values[key]['transactionTime']}'),
+                        subtitle: Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                    '${values[key]['transactionDescription']}'),
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 5),
+                                    child: values[key]['transactionType'] ==
+                                            'expense'
+                                        ? RichText(
+                                            text: TextSpan(
+                                              style:
+                                                  DefaultTextStyle.of(context)
+                                                      .style,
+                                              children: [
+                                                const WidgetSpan(
+                                                  child: Icon(
+                                                    Icons.remove,
+                                                    size: 16,
+                                                    color: Colors.red,
+                                                  ),
+                                                ),
+                                                TextSpan(
+                                                  text:
+                                                      "${values[key]['transactionAmount']}",
+                                                  style: TextStyle(
+                                                      color: Colors.black
+                                                          .withOpacity(0.9)),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        : RichText(
+                                            text: TextSpan(
+                                              style:
+                                                  DefaultTextStyle.of(context)
+                                                      .style,
+                                              children: [
+                                                const WidgetSpan(
+                                                  child: Icon(
+                                                    Icons.add,
+                                                    size: 16,
+                                                    color: Colors.green,
+                                                  ),
+                                                ),
+                                                TextSpan(
+                                                  text:
+                                                      "${values[key]['transactionAmount']}",
+                                                  style: TextStyle(
+                                                      color: Colors.black
+                                                          .withOpacity(0.9)),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 5),
+                                    child: RichText(
+                                      text: TextSpan(
+                                        style:
+                                            DefaultTextStyle.of(context).style,
+                                        children: [
+                                          const TextSpan(
+                                            text: "previous balance: ",
+                                            style:
+                                                TextStyle(color: Colors.orange),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                "${values[key]['balanceBeforeTransaction']}",
+                                            style: TextStyle(
+                                                color: Colors.black
+                                                    .withOpacity(0.9)),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 5.0),
+                                    child: RichText(
+                                      text: TextSpan(
+                                        style:
+                                            DefaultTextStyle.of(context).style,
+                                        children: [
+                                          const TextSpan(
+                                            text: "current balance: ",
+                                            style:
+                                                TextStyle(color: Colors.green),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                "${values[key]['balanceAfterTransaction']}",
+                                            style: TextStyle(
+                                                color: Colors.black
+                                                    .withOpacity(0.9)),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                  title: Text(
-                      '${values[key]['transactionDate']} @ ${values[key]['transactionTime']}'),
-                  subtitle: Column(
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Text('${values[key]['transactionDescription']}'),
-                        ],
                       ),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 5),
-                              child: values[key]['transactionType'] == 'expense'
-                                  ? RichText(
-                                      text: TextSpan(
-                                        style:
-                                            DefaultTextStyle.of(context).style,
-                                        children: [
-                                          const WidgetSpan(
-                                            child: Icon(
-                                              Icons.remove,
-                                              size: 16,
-                                              color: Colors.red,
-                                            ),
-                                          ),
-                                          TextSpan(
-                                            text:
-                                                "${values[key]['transactionAmount']}",
-                                            style: TextStyle(
-                                                color: Colors.black
-                                                    .withOpacity(0.9)),
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  : RichText(
-                                      text: TextSpan(
-                                        style:
-                                            DefaultTextStyle.of(context).style,
-                                        children: [
-                                          const WidgetSpan(
-                                            child: Icon(
-                                              Icons.add,
-                                              size: 16,
-                                              color: Colors.green,
-                                            ),
-                                          ),
-                                          TextSpan(
-                                            text:
-                                                "${values[key]['transactionAmount']}",
-                                            style: TextStyle(
-                                                color: Colors.black
-                                                    .withOpacity(0.9)),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 5),
-                              child: RichText(
-                                text: TextSpan(
-                                  style: DefaultTextStyle.of(context).style,
-                                  children: [
-                                    const TextSpan(
-                                      text: "previous balance: ",
-                                      style: TextStyle(color: Colors.orange),
-                                    ),
-                                    TextSpan(
-                                      text:
-                                          "${values[key]['balanceBeforeTransaction']}",
-                                      style: TextStyle(
-                                          color: Colors.black.withOpacity(0.9)),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 5.0),
-                              child: RichText(
-                                text: TextSpan(
-                                  style: DefaultTextStyle.of(context).style,
-                                  children: [
-                                    const TextSpan(
-                                      text: "current balance: ",
-                                      style: TextStyle(color: Colors.green),
-                                    ),
-                                    TextSpan(
-                                      text:
-                                          "${values[key]['balanceAfterTransaction']}",
-                                      style: TextStyle(
-                                          color: Colors.black.withOpacity(0.9)),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(
+                    thickness: 3,
                   ),
                 ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(
-              thickness: 3,
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
