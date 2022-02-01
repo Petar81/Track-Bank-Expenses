f7b8f77388a0bb430615e02f996eeb9504d48fb2
Petar <baki_pfc@yahoo.com>
2022-02-02 00:23:03 +0100

    add line chart to trend screen and populate it with a dummy data

--- lib/screens/trend.dart
+++ lib/screens/trend.dart
@@ -15,8 +15,48 @@ class _TrendState extends State<Trend> {
       appBar: AppBar(
         title: const Text('Trend overview'),
       ),
-      body: const Center(
-        child: Text('Trend Screen'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0, bottom: 10.0),
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            width: double.infinity,
+            child: LineChart(
+              LineChartData(
+                  borderData: FlBorderData(show: false),
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: [
+                        const FlSpot(3, 130.01),
+                        const FlSpot(6, 57.22),
+                        const FlSpot(7, 150.00),
+                      ],
+                      isCurved: false,
+                      barWidth: 3,
+                      colors: [
+                        Colors.green.shade300,
+                      ],
+                    ),
+                    LineChartBarData(
+                      spots: [
+                        const FlSpot(1, 79.23),
+                        const FlSpot(2, 900),
+                        const FlSpot(2, 13.11),
+                        const FlSpot(3, 47.99),
+                        const FlSpot(6, 17.56),
+                        const FlSpot(1, 15.00),
+                        const FlSpot(7, 20.00)
+                      ],
+                      isCurved: false,
+                      barWidth: 3,
+                      colors: [
+                        Colors.orange.shade300,
+                      ],
+                    ),
+                  ]),
+            ),
+          ),
+        ),
       ),
     );
   }
