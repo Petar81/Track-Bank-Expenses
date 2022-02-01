7dc5c4a382465a54aadd2fa2aa0e7a095fd17a7d
Petar <baki_pfc@yahoo.com>
2022-02-01 03:40:28 +0100

    change trend widget from stateless to stateful

--- lib/screens/trend.dart
+++ lib/screens/trend.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Trend extends StatelessWidget {
+class Trend extends StatefulWidget {
   const Trend({Key? key}) : super(key: key);
 
+  @override
+  State<Trend> createState() => _TrendState();
+}
+
+class _TrendState extends State<Trend> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
