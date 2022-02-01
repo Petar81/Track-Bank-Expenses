058ee64a5a21139ed4db75a864e3ff35af403a35
Petar <baki_pfc@yahoo.com>
2022-02-01 21:35:56 +0100

    switch charts_flutter with fl_chart library

--- lib/screens/trend.dart
+++ lib/screens/trend.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:fl_chart/fl_chart.dart';
 
 class Trend extends StatefulWidget {
   const Trend({Key? key}) : super(key: key);
