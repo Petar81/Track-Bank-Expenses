e0b9c6beb82626fa0cf79100bfdf1595ad31fa97
Petar <baki_pfc@yahoo.com>
2022-02-03 02:35:31 +0100

    get the last 7 transactions; filter expenses only and add it to chart spots

--- lib/screens/trend.dart
+++ lib/screens/trend.dart
@@ -18,6 +18,16 @@ class _TrendState extends State<Trend> {
 
   Map<dynamic, dynamic> values = {};
   bool _isLoading = true;
+  List keys = [];
+  List<FlSpot> expenseSpots = [
+    const FlSpot(1, 0),
+    const FlSpot(2, 0),
+    const FlSpot(3, 0),
+    const FlSpot(4, 0),
+    const FlSpot(5, 100),
+    const FlSpot(6, 0),
+    const FlSpot(7, 0)
+  ];
 
   getTransactions() async {
     Query ref = FirebaseDatabase.instance.ref("transactions").limitToLast(7);
@@ -27,6 +37,20 @@ class _TrendState extends State<Trend> {
 
     Map<dynamic, dynamic> data = event.snapshot.value as Map<dynamic, dynamic>;
     values = data;
+
+    keys = (values.keys.toList()..sort());
+    //print(keys);
+
+    for (var i = 0; i < keys.length; i++) {
+      if (values[keys[i]]['transactionType'] == 'expense') {
+        //print(values[keys[i]]['transactionType']);
+        var value = values[keys[i]]['transactionAmount'];
+        value = value + .0;
+        //print(value.runtimeType);
+        expenseSpots[i] = FlSpot(i + 1, value);
+        //print(expenseSpots.toString());
+      }
+    }
 
     setState(() {
       _isLoading = !_isLoading;
@@ -70,15 +94,7 @@ class _TrendState extends State<Trend> {
                       ],
                     ),
                     LineChartBarData(
-                      spots: [
-                        const FlSpot(1, 150.23),
-                        const FlSpot(2, 99),
-                        const FlSpot(3, 47.99),
-                        const FlSpot(4, 0),
-                        const FlSpot(5, 0),
-                        const FlSpot(6, 457.56),
-                        const FlSpot(7, 20.00)
-                      ],
+                      spots: expenseSpots,
                       isCurved: false,
                       barWidth: 3,
                       colors: [
