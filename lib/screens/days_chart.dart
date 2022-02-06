eaed779da173729102a17bdf0983155e23ca405b
Petar <baki_pfc@yahoo.com>
2022-02-06 03:19:27 +0100

    delete inline Weekdays class/data model from days_chart screen

--- lib/screens/days_chart.dart
+++ lib/screens/days_chart.dart
@@ -29,14 +29,3 @@ class DaysChart extends StatelessWidget {
     );
   }
 }
-
-/// Sample ordinal data type.
-class WeekDays {
-  final String day;
-  final double sum;
-  final charts.Color color;
-
-  WeekDays(this.day, this.sum, Color color)
-      : color = charts.Color(
-            r: color.red, g: color.green, b: color.blue, a: color.alpha);
-}
