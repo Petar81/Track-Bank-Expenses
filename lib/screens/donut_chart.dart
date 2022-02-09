21e540a7e6e47010982c4445ce9b0f5f53b0b8d2
Petar <baki_pfc@yahoo.com>
2022-02-09 22:11:42 +0100

    refactor the body of the donut_chart screen

--- lib/screens/donut_chart.dart
+++ lib/screens/donut_chart.dart
@@ -33,33 +33,61 @@ class DonutChart extends StatelessWidget {
           ],
         ),
       ),
-      body: PieChart(
-        dataMap: dataMap,
-        animationDuration: const Duration(milliseconds: 1200),
-        chartLegendSpacing: 122,
-        chartRadius: MediaQuery.of(context).size.width / 3.2,
-        colorList: colorList,
-        initialAngleInDegree: 0,
-        chartType: ChartType.ring,
-        ringStrokeWidth: 182,
-        centerText: "%",
-        legendOptions: const LegendOptions(
-          showLegendsInRow: true,
-          legendPosition: LegendPosition.bottom,
-          showLegends: true,
-          legendTextStyle: TextStyle(
-            fontWeight: FontWeight.bold,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 80),
+                child: PieChart(
+                  dataMap: dataMap,
+                  animationDuration: const Duration(milliseconds: 1200),
+                  chartLegendSpacing: 122,
+                  chartRadius: MediaQuery.of(context).size.width / 3.2,
+                  colorList: colorList,
+                  initialAngleInDegree: 0,
+                  chartType: ChartType.ring,
+                  ringStrokeWidth: 182,
+                  centerText: "%",
+                  legendOptions: const LegendOptions(
+                    showLegendsInRow: true,
+                    legendPosition: LegendPosition.bottom,
+                    showLegends: true,
+                    legendTextStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  chartValuesOptions: const ChartValuesOptions(
+                    showChartValueBackground: true,
+                    showChartValues: true,
+                    showChartValuesInPercentage: true,
+                    showChartValuesOutside: true,
+                    decimalPlaces: 1,
+                  ),
+                  // gradientList: ---To add gradient colors---
+                  // emptyColorGradient: ---Empty Color gradient---
+                ),
+              ),
+              Row(
+                children: const [
+                  Text('8797.89'),
+                  Padding(
+                    padding: EdgeInsets.only(right: 20, bottom: 80),
+                  ),
+                  Text('76768.67'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text('TOTAL = 29898.76'),
+                ],
+              ),
+            ],
           ),
-        ),
-        chartValuesOptions: const ChartValuesOptions(
-          showChartValueBackground: true,
-          showChartValues: true,
-          showChartValuesInPercentage: true,
-          showChartValuesOutside: true,
-          decimalPlaces: 1,
-        ),
-        // gradientList: ---To add gradient colors---
-        // emptyColorGradient: ---Empty Color gradient---
+        ],
       ),
     );
   }
