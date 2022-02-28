ff219f4d19f7c752e55462fd744108449e0cc2f3
Petar <baki_pfc@yahoo.com>
2022-02-28 14:53:34 +0100

    define initState, onStart and getLastNTransactions functions

--- lib/screens/trend.dart
+++ lib/screens/trend.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class Trend extends StatefulWidget {
   final List<FlSpot> expenseSpots;
@@ -13,10 +14,81 @@ class Trend extends StatefulWidget {
 
 class _TrendState extends State<Trend> {
   @override
+  void initState() {
+    super.initState();
+    onStart();
+  }
+
+  bool _isLoading = true;
+  String _transactionQueryStr = 'last 10 transactions';
+  var expenseSpots = <FlSpot>[];
+  var depositSpots = <FlSpot>[];
+
+  onStart() async {
+    // IT HOLDS LOGIC FOR LAST 10 TRANSACTIONS CHART
+    await getLastNTransactions(10);
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  getLastNTransactions(int numberOfTransactions) async {
+    int transactionsNumber = numberOfTransactions;
+    Map<dynamic, dynamic> values = {};
+    var keys = [];
+    var newExpenseSpots = <FlSpot>[];
+    var newDepositSpots = <FlSpot>[];
+
+    Query ref = FirebaseDatabase.instance
+        .ref("transactions")
+        .limitToLast(transactionsNumber);
+
+// Get the data once
+    DatabaseEvent event = await ref.once();
+
+    Map<dynamic, dynamic> data = event.snapshot.value as Map<dynamic, dynamic>;
+    values = data;
+
+    keys = (values.keys.toList()..sort());
+    //print(keys);
+
+    for (var i = 0; i < keys.length; i++) {
+      if (values[keys[i]]['transactionType'] == 'expense') {
+        //print(values[keys[i]]['transactionType']);
+        var value = values[keys[i]]['transactionAmount'];
+        value = value + .0;
+        //print(value.runtimeType);
+        newExpenseSpots.add(FlSpot(i + 1, value));
+        //print(expenseSpots.toString());
+      } else {
+        newExpenseSpots.add(FlSpot(i + 1, 0.0));
+      }
+    }
+
+    for (var i = 0; i < keys.length; i++) {
+      if (values[keys[i]]['transactionType'] == 'deposit') {
+        //print(values[keys[i]]['transactionType']);
+        var value = values[keys[i]]['transactionAmount'];
+        value = value + .0;
+        //print(value.runtimeType);
+        newDepositSpots.add(FlSpot(i + 1, value));
+        //print(expenseSpots.toString());
+      } else {
+        newDepositSpots.add(FlSpot(i + 1, 0.0));
+      }
+    }
+
+    setState(() {
+      expenseSpots = newExpenseSpots;
+      depositSpots = newDepositSpots;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Trend for last 10 transactions'),
+        title: Text(_transactionQueryStr),
       ),
       body: Center(
         child: Padding(
