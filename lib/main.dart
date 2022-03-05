c241d32ffbea2b96a53dda718d426d4f772935f1
Petar <baki_pfc@yahoo.com>
2022-03-05 05:01:13 +0100

    import firebase_options.dart into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 import './screens/balance_overview.dart';
 
