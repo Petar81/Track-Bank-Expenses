fd1dde8733d0da034c6729bb9906aed16a22c820
Petar <baki_pfc@yahoo.com>
2022-03-06 02:46:29 +0100

    refactor myApp() logic

--- lib/main.dart
+++ lib/main.dart
@@ -4,54 +4,39 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 import './screens/balance_overview.dart';
+import './screens/user_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bank expense tracker',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-        future: _fbApp,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            // ignore: avoid_print
-            // print('You have an error ${snapshot.error.toString()}');
-            return const Text('Something went wrong!');
-          } else if (snapshot.hasData) {
-            // ignore: avoid_print
-            // print('You got Data');
-            return const BalanceOverview(title: 'Bank expense tracker');
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-    );
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+
+    return (user != null && !user.emailVerified)
+        ? MaterialApp(
+            title: 'Bank expense tracker',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const BalanceOverview(title: 'Bank expense tracker'),
+          )
+        : MaterialApp(
+            title: 'Bank expense tracker',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const UserAuth(),
+          );
   }
 }
