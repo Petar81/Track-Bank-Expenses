0d25fe6bcd39befdf17b117df894ff8755a7e9ee
Petar <baki_pfc@yahoo.com>
2022-03-07 23:44:38 +0100

    add form title and submit button

--- lib/screens/user_auth.dart
+++ lib/screens/user_auth.dart
@@ -19,11 +19,13 @@ class _UserAuthState extends State<UserAuth> {
       body: Center(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Form(
                 key: _formKey,
                 child: Column(
                   children: <Widget>[
+                    const Text("SIGNUP FORM"),
                     TextFormField(
                       // The validator receives the text that the user has entered.
                       validator: (value) {
@@ -32,6 +34,22 @@ class _UserAuthState extends State<UserAuth> {
                         }
                         return null;
                       },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            // If the form is valid, display a snackbar. In the real world,
+                            // you'd often call a server or save the information in a database.
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
+                            );
+                          }
+                        },
+                        child: const Text('Submit'),
+                      ),
                     ),
                   ],
                 ),
