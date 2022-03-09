79b3050530e137cae121aaf18bda5514a2a57127
Petar <baki_pfc@yahoo.com>
2022-03-09 18:01:49 +0100

    update border styles of the input firlds

--- lib/models/input_decoration.dart
+++ lib/models/input_decoration.dart
@@ -6,20 +6,20 @@ InputDecoration buildInputDecoration(IconData icons, String hinttext) {
     prefixIcon: Icon(icons),
     focusedBorder: OutlineInputBorder(
       borderRadius: BorderRadius.circular(25.0),
-      borderSide: const BorderSide(color: Colors.green, width: 1.5),
+      borderSide: const BorderSide(color: Colors.purple, width: 2.5),
     ),
     border: OutlineInputBorder(
       borderRadius: BorderRadius.circular(25.0),
       borderSide: const BorderSide(
         color: Colors.blue,
-        width: 1.5,
+        width: 2.5,
       ),
     ),
     enabledBorder: OutlineInputBorder(
       borderRadius: BorderRadius.circular(25.0),
       borderSide: const BorderSide(
         color: Colors.blue,
-        width: 1.5,
+        width: 2.5,
       ),
     ),
   );
