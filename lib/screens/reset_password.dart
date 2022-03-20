6b5ddb8cf0299f5cb54a72549f7d42b42c393ce5
Petar <baki_pfc@yahoo.com>
2022-03-20 01:17:54 +0100

    add reset_password screen

---
+++ lib/screens/reset_password.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Track Bank Expenses'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[Text('under construction')],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
