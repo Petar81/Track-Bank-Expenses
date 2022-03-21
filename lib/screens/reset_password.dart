eda3b0c66b466cb172bddddc8c553c710af8e64e
Petar <baki_pfc@yahoo.com>
2022-03-21 23:14:10 +0100

    add submit button and logic that goes with it

--- lib/screens/reset_password.dart
+++ lib/screens/reset_password.dart
@@ -10,7 +10,6 @@ class ResetPassword extends StatefulWidget {
 
 class _ResetPasswordState extends State<ResetPassword> {
   final _resetFormKey = GlobalKey<FormState>();
-
   String loginEmail = '';
 
   @override
@@ -28,35 +27,70 @@ class _ResetPasswordState extends State<ResetPassword> {
               children: <Widget>[
                 Form(
                   key: _resetFormKey,
-                  child: Column(children: <Widget>[
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 10.0),
-                      child: Text(
-                        "RESET PASSWORD",
-                        style: TextStyle(fontSize: 20.0),
+                  child: Column(
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: Text(
+                          "RESET PASSWORD",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: TextFormField(
-                        keyboardType: TextInputType.text,
-                        decoration: buildInputDecoration(Icons.email, "Email"),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter an email';
-                          }
-                          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                              .hasMatch(value)) {
-                            return 'Please enter a valid email';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                          loginEmail = value!;
-                        },
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
+                          decoration:
+                              buildInputDecoration(Icons.email, "Email"),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter an email';
+                            }
+                            if (!RegExp(
+                                    "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                                .hasMatch(value)) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            loginEmail = value!;
+                          },
+                        ),
                       ),
-                    ),
-                  ]),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            // Validate returns true if the form is valid, or false otherwise.
+                            if (_resetFormKey.currentState!.validate()) {
+                              _resetFormKey.currentState!.save();
+                              // If the form is valid, display a snackbar. In the real world,
+                              // you'd often call a server or save the information in a database.
+                              ScaffoldMessenger.of(context)
+                                ..hideCurrentSnackBar()
+                                ..showSnackBar(
+                                  const SnackBar(
+                                      duration: Duration(seconds: 2),
+                                      content: Text('Processing Data')),
+                                );
+                              await Future.delayed(const Duration(seconds: 1));
+                              () async {
+                                try {
+                                  await FirebaseAuth.instance
+                                      .sendPasswordResetEmail(
+                                          email: loginEmail);
+                                } on FirebaseAuthException catch (e) {
+                                  return e.message;
+                                }
+                              }();
+                            }
+                          },
+                          child: const Text('Submit'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
