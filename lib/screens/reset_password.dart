c024d8fd254e143bd37aa8a8386fcb86f23e158e
Petar <baki_pfc@yahoo.com>
2022-03-21 22:54:50 +0100

    add email TextFormField

--- lib/screens/reset_password.dart
+++ lib/screens/reset_password.dart
@@ -19,12 +19,32 @@ class ResetPassword extends StatelessWidget {
               children: <Widget>[
                 Form(
                   key: _resetFormKey,
-                  child: Column(children: const <Widget>[
-                    Padding(
+                  child: Column(children: <Widget>[
+                    const Padding(
                       padding: EdgeInsets.only(bottom: 10.0),
                       child: Text(
                         "RESET PASSWORD",
                         style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration: buildInputDecoration(Icons.email, "Email"),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email';
+                          }
+                          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                              .hasMatch(value)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          loginEmail = value!;
+                        },
                       ),
                     ),
                   ]),
