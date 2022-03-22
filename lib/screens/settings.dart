02c98a0aebdac81d043105caedcad78f297b945a
Petar <baki_pfc@yahoo.com>
2022-03-23 00:50:12 +0100

    add settings.dart screen

---
+++ lib/screens/settings.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Track Bank Expenses'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[Text('under construction')],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
